bcf6cd61da886af902a79168180d5a90d3e7f79e
Pauli Matila <pauli.matila@outlook.com>
2020-03-31 14:42:27 +0300

    Add project files.

---
+++ MetodiHarjotus/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetodiHarjotus
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public List<string> Hobbies = new List<string>();
+        public Person Friend;
+
+
+        public Person()
+        {
+
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine($"Terve! miten menee? {Name}");
+        }
+
+        public void FormanGreet()
+        {
+            Console.WriteLine($"Terve! {Name}. Jolla on ikää {Age} ja harrastan {Hobbies[0]}");
+        }
+
+        public bool AgeCheck()
+        {
+            if (Age < 18)
+            {
+                FormanGreet();
+                Console.WriteLine($"Hei! nimeni on {Name}. Olen alaikäinen");
+                return false;
+            }
+            else
+            {
+                FormanGreet();
+                Console.WriteLine($"Hei! nimeni on {Name}. Olen täysi-ikäinen");
+                return true;
+            }
+        }
+
+        public void GrowingOld(int years)
+        {
+            Age += years;
+        }
+
+        public string FriendCompare()
+        {
+            if (Friend == null)
+            {
+                return $"{Name}. Olet yksinäinen";
+            }
+            if (Age > Friend.Age)
+            {
+                return $"{Name}. Sinä olet vanhempi";
+                
+            }
+            else
+            {
+                return $"{Name}. Sinä olet vanhempi";
+            }
+        }
+    }
+}
---
+++ MetodiHarjotus/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MetodiHarjotus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person();
+            Person person3 = new Person();
+            person1.Age = 16;
+            person2.Age = 19;
+            person3.Age = 24;
+            person1.Name = "Mikko";
+            person2.Name = "Kalle";
+            person3.Name = "Tiina";
+            person1.Hobbies.Add("Golf, Tennis");
+            person2.Hobbies.Add("Jalkapallo");
+            person3.Hobbies.Add("Uinti, Pianonsoitto, Jääkiekko");
+            person1.Friend = person2;
+            person2.Friend = person1;
+            person1.Greet();
+            person1.FormanGreet();
+            person3.FormanGreet();
+            person2.FormanGreet();
+            person1.AgeCheck();
+            person3.AgeCheck();
+            person1.GrowingOld(10);
+            person1.AgeCheck();
+            Console.WriteLine(person1.FriendCompare());
+            Console.WriteLine(person3.FriendCompare());
+        }   
+    }
+}
